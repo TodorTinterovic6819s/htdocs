3e7e81b9137fb806885849e926efbe0a0f4d81b9
Todor Tinterovic 6819s <ttinterovic6819s@raf.rs>
2020-10-14 04:22:11 +0200

    Initial commit

---
+++ 1.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "Todor <br> Tinterovic <br> 68/19 <br><br>";
+    
+    echo "Jovan <br> jovanovic <br> 76/19 <br><br>";
+    
+    echo "Marko <br> Markovic <br> 34/18 <br><br>";
+
+
+?>
